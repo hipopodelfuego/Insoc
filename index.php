eb9fe003b902e5c6fcd94cf1890b2202b82475b9
Laïcan <167437065+laican-fr@users.noreply.github.com>
2024-09-26 10:58:08 +0200

    Font + static + fin squelette

--- index.php
+++ index.php
@@ -13,17 +13,8 @@ session_start();
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <meta charset="utf-8">
         <title>Vote en ligne</title>
-        <link rel="icon" href="css/img/general/icon.png" >
-        <meta name="author" content="Insoc">
-        <meta name="keywords" content="">
-        <meta name="description" content="" />
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <?php include("static/head.php")?>
     </head>
     <body>
         <?php include("static/header.php"); ?>
---
+++ static/head.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<link rel="icon" href="css/img/favicon.svg" >
+<meta name="author" content="Morgane Théault | Hippolyte Tribut | Aude Cimaz | Corentin Durand | Damien Vailland">
+<meta name="description" content="Site fictif représentant ce que pourrait être une plateforme française de vote en ligne" />
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<meta content="width=device-width, initial-scale=1" name="viewport" />
