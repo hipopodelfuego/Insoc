fa66d3875f21a06c5be2e5a8895821f54734f463
Laïcan <167437065+laican-fr@users.noreply.github.com>
2024-10-10 10:44:26 +0200

    footer + register

--- static/footer.php
+++ static/footer.php
@@ -1,2 +1,10 @@
-<footer class="footer">
+<footer id="footer">
+    <section class='firstFoot'>
+        <a href='index.php' class='navTitleContainer'>
+            <img class='logoRF' src='css/img/Logo_RF.svg'>
+        </a>
+        <div>
+            Ce site est un quoicoubaka
+        </div>
+    </section>
 </footer>
