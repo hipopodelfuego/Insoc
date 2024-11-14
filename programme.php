d5efa7babf82bf5afd38174174f0e618c5684ece
CorentinDurand <corentin.durand309@gmail.com>
2024-11-14 10:04:58 +0100

    Update programme.php

--- programme.php
+++ programme.php
@@ -15,12 +15,27 @@ session_start();
     <head>
         <title>Programme</title>
         <?php include("static/head.php")?>
+        <style>
+            .pdf-container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
+            }
+            .pdf-viewer {
+                width: 100%; 
+                max-width: 800px; 
+                height: 700px;
+                box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5); 
+            }
+        </style>
     </head>
     <body>
         <?php include("static/header.php"); ?>
         <main>
-            <div style="width: 100%; height: 480px;" class="smallpdf-widget" data-pdf-url="css/pdf/arthaud.pdf"></div>
-            <script src="https://smallpdf.com/api/embed-widget.js"></script>
+            <div class="pdf-container">
+                <embed src="<?php echo 'css/pdf/arthaud.pdf'; ?>" type="application/pdf" class="pdf-viewer">
+            </div>
         </main>
         <?php include("static/footer.php"); ?>
         <script src="js/index.js"></script>
